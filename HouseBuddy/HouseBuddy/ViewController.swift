d65cd822885cc692504cf0c1e4dc65df8b94ddb1
Unknown <alexandar1000@gmail.com>
2018-11-22 16:59:57 +0100

    Initial commit with the project structure

---
+++ HouseBuddy/HouseBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HouseBuddy
+//
+//  Created by A.S. Janjanin on 22/11/2018.
+//  Copyright © 2018 HouseBuddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

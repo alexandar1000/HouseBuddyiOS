af1f4746362f19f2276ba7a5598d4cdec032034d
Aleksandar Sasa <alexandar1000@gmail.com>
2019-01-28 21:59:00 +0100

    Added the front end - controllers. Next step, add the logic behind the expense tracker

---
+++ HouseBuddy/HouseBuddy/ExpenseTracker/EditExpenseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditExpenseViewController.swift
+//  HouseBuddy
+//
+//  Created by Aleksandar Sasa on 28/01/2019.
+//  Copyright © 2019 HouseBuddy. All rights reserved.
+//
+
+import UIKit
+
+class EditExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HouseBuddy/HouseBuddy/ExpenseTracker/ShowExpenseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowExpenseViewController.swift
+//  HouseBuddy
+//
+//  Created by Aleksandar Sasa on 28/01/2019.
+//  Copyright © 2019 HouseBuddy. All rights reserved.
+//
+
+import UIKit
+
+class ShowExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

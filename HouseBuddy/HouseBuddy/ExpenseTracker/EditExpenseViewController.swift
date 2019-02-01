eca47beac30309e1de8e3ed11ec2ff1621915121
Aleksandar Sasa <alexandar1000@gmail.com>
2019-02-01 23:09:19 +0100

    Implemented a bit of backend logic

--- HouseBuddy/HouseBuddy/ExpenseTracker/EditExpenseViewController.swift
+++ HouseBuddy/HouseBuddy/ExpenseTracker/EditExpenseViewController.swift
@@ -11,25 +11,58 @@ import UIKit
 class EditExpenseViewController: UIViewController {
 
 	//MARK: - Outlets
-	
+    @IBOutlet weak var dateInput: UITextField!
+    @IBOutlet weak var priceInput: UITextField!
+    @IBOutlet weak var titleInput: UITextField!
+    @IBOutlet weak var descriptionInput: UITextField!
+    
+    
+    //MARK: - Fields
+    private var datePicker: UIDatePicker?
+    private var df: DateFormatter = DateFormatter()
+    
 	//MARK: - Fields
-	var expense: ExpenseEntry? = ExpenseEntry();
+	var expense: ExpenseEntry? = nil
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        df.dateFormat = "dd.MM.yyyy"
+        
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(datePickerChanged(_:)), for: .valueChanged)
+        
+        dateInput.inputView = datePicker
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func datePickerChanged(_ sender: UIDatePicker) {
+        dateInput.text = df.string(from: sender.date)
     }
-    */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
 
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let date = dateInput.text else {
+            return
+        }
+        guard let price = priceInput.text else {
+            return
+        }
+        guard let name = titleInput.text else {
+            return
+        }
+        guard let description = descriptionInput.text else {
+            return
+        }
+        
+        let convertedDate: Date = df.date(from: date) ?? Date.init()
+        let convertedPrice: Double = Double(price) ?? 0
+        
+        expense = ExpenseEntry(name: name, description: description, price: convertedPrice, date: convertedDate)
+    }
 }
